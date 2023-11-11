1f51057deddc0f5e9eb70b638fd9c52050367aa3
Martinus Rombe <55009563+martin1021@users.noreply.github.com>
2023-11-11 23:27:05 +0800

    Coba ngikutin doc animate.css

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app" class="container mt-5">
     <h1>IDShop</h1>
-    <p class="animate__fadeInRight">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Amet unde sequi non quisquam facilis dicta nesciunt earum
+    <p class="animate__animated animate__fadeInRight animate__delay-2s">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Amet unde sequi non quisquam facilis dicta nesciunt earum
       nostrum illum eos facere, distinctio, similique, quo assumenda eligendi? Doloremque adipisci minus consectetur!</p>
       <font-awesome-icon icon="shopping-cart"></font-awesome-icon>
   </div>
