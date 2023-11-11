014791d262d2859c3973c0212dc66b4d12a1935c
Martinus Rombe <55009563+martin1021@users.noreply.github.com>
2023-11-11 21:04:29 +0800

    menambahkan file

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app" class="container mt-5">
+    <h1>IDShop</h1>
+    <p class="animate__fadeInRight">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Amet unde sequi non quisquam facilis dicta nesciunt earum
+      nostrum illum eos facere, distinctio, similique, quo assumenda eligendi? Doloremque adipisci minus consectetur!</p>
+      <font-awesome-icon icon="shopping-cart"></font-awesome-icon>
+  </div>
+</template>
+
+<script>
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+export default {
+  name: "App",
+  components: {
+    FontAwesomeIcon,
+  },
+};
+</script>
+
+
